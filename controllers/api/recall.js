80a40daef6f0a335c3d27d8ae6b10a58a04daaa2
M. Adam Kendall <akendall@devis.com>
2015-06-23 11:16:08 -0400

    Update recall API controller to take advantage of services

--- controllers/api/recall.js
+++ controllers/api/recall.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var OpenFdaApiService = require('../../services/OpenFdaApiService');
+
 module.exports = function(router) {
 
     router.get('/:lat/:lon', function(req, res) {
-        res.json({meta: { last_updated: '2015-05-31'}});
+        OpenFdaApiService.getRecallsByLatLong(req.params.lat, req.params.lon, req.query.limit, req.query.skip)
+        .then(function(result) {
+            res.json(result);
+        })
+        .catch(function(err) {
+            res.status(500).json({msg: err});
+        });
     });
 
 };
--- test/spec/server/controllers/api/recallSpec.js
+++ test/spec/server/controllers/api/recallSpec.js
@@ -1,15 +1,40 @@
-/*global describe:false, it:false, beforeEach:false, afterEach:false, request:false, mock:false*/
+/*global describe:false, it:false, beforeEach:false, afterEach:false, request:false, mock:false, nock:false*/
 
 'use strict';
 
 describe('/api/recall/:lat/:lon', function() {
 
-    it('should return data when passed a lat and lon', function(done) {
+    it('should return data when passed a valid lat and lon', function(done) {
+
+        nock('https://data.fcc.gov')
+        .get('/api/block/find?format=json&latitude=37&longitude=-80')
+        .reply(200, require('../../../../data/fcc-lat-lon-va.json'));
+
+        nock('https://api.fda.gov')
+        .get('/food/enforcement.json?limit=100&skip=0&search=status%3A%22Ongoing%22%20AND%20(distribution_pattern%3A%22nationwide%22%20distribution_pattern%3A%22VA%22)')
+        .reply(200, require('../../../../data/food-recalls-va.json'));
+
         request(mock)
-            .get('/api/recall/1/2')
+            .get('/api/recall/37/-80')
             .expect(200)
             .expect('Content-Type', /json/)
             .expect(/"last_updated"/)
+            .end(function(err, res) {
+                done(err);
+            });
+    });
+
+    it('should return 500 when passed an invalid lat and lon', function(done) {
+
+        nock('https://data.fcc.gov')
+        .get('/api/block/find?format=json&latitude=37&longitude=80')
+        .reply(200, require('../../../../data/fcc-lat-lon-null.json'));
+
+        request(mock)
+            .get('/api/recall/37/80')
+            .expect(500)
+            .expect('Content-Type', /json/)
+            .expect(/"msg"/)
             .end(function(err, res) {
                 done(err);
             });
